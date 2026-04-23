a515e6af8eda8a106e6e64573333f306d230cb79
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-23 18:28:14 +0530

    Refactor App component to include Card and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react'
+import Card from './comp/Card'
+const App = () => {
+    return (
+        <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",gap:20}}>
+            <section className='review-heading' >
+                <h1 style={{fontSize:24,fontWeight:600}} >Our Review </h1>
+            </section>
+            <Card />
+        </div>
+    )
+}
 
+export default App
